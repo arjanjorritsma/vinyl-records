9f8c6f9debebe627f279575d9393adebb4c5838f
arjanjorritsma <arjanjorritsma@gmail.com>
2015-11-01 10:35:29 +0100

    Spring data rest toegevoegd

--- src/main/java/nl/arjan/vinylrecords/domain/repository/ArtistRepository.java
+++ src/main/java/nl/arjan/vinylrecords/domain/repository/ArtistRepository.java
@@ -3,7 +3,9 @@ package nl.arjan.vinylrecords.domain.repository;
 import nl.arjan.vinylrecords.domain.Artist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "artist")
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
-
+	public Artist findByName(String name);
 }
--- src/main/java/nl/arjan/vinylrecords/domain/repository/RecordRepository.java
+++ src/main/java/nl/arjan/vinylrecords/domain/repository/RecordRepository.java
@@ -3,7 +3,9 @@ package nl.arjan.vinylrecords.domain.repository;
 import nl.arjan.vinylrecords.domain.Record;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "record")
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 }
